62ef75951650692eeaf9a98bfb84ee349af63a73
Jimmy9812 <wladimirmaila1998pm@gmail.com>
2025-01-30 21:52:02 -0500

    feat: :sparkles:

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -17,7 +17,7 @@ const router = express.Router();
 router.get("/health", (req, res) => {
   return res.status(200).json({
     status: "OK",
-    message: "Hola Juanpablo",
+    message: "CONEXION EXISTOSA",
   });
 });
 
